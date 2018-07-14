72ba06278a6e7b93e40e872989ed4b74073449ff
AZCoder018 <jason.meadows.esq@hotmail.com>
2018-07-13 23:59:49 -0700

    revisions to Header.js and Header.css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./Header.css";
 
-// create a header with a Bootstrap navbar and jumbotron
+//Create Bootstrap navbar and jumbotron
 const Header = props => (
   
   <div className="main-container">
     <nav className="navbar sticky-top navbar-light bg-light justify-content-end">
-      <h5 className="text-muted">Current game score: {props.currentScore} | Top score: {props.topScore}</h5>
+      <h5 className="text">Current game score: {props.currentScore} | Top score: {props.topScore}</h5>
     </nav>
     
-    <div className="jumbotron text-muted">
+    <div className="jumbotron">
       <h1 className="title">Memory Card Game</h1>
       <p className="lead">Click a card, but only click it once!</p>
     </div>
